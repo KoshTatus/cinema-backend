0001352b3aab8ada1f98373de2c2d5c20bab184b
KoshTatus <153766064+KoshTatus@users.noreply.github.com>
2025-04-13 19:49:23 +0400

    feat: create tables

--- src/models.py
+++ src/models.py
@@ -1,12 +1,13 @@
-from sqlalchemy import ForeignKey, Enum, Date
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+import datetime
+from typing import List
 
-from enums import Genre, AgeRating
+from sqlalchemy import ForeignKey, Enum, Date, DateTime, JSON
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 
+from enums import AgeRating
 
 class Base(DeclarativeBase):
     pass
-
 
 class UsersOrm(Base):
     __tablename__ = "users"
@@ -21,18 +22,20 @@ class MoviesOrm(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str]
     genre: Mapped[int] = mapped_column(ForeignKey("genres.id"))
-    age_rating: Mapped[Enum[AgeRating]]
-    date: Mapped[Date]
+    age_rating: Mapped[AgeRating]
     director: Mapped[str]
     screenwriter: Mapped[str]
-    actors: Mapped[list[str]]
-    annotation: Mapped[str]
+    actors: Mapped[list[str]] = mapped_column(JSON)
+    description: Mapped[str]
+    trailer_url: Mapped[str]
+    poster_url: Mapped[str]
 
 
 class SessionsOrm(Base):
     __tablename__ = "sessions"
     id: Mapped[int] = mapped_column(primary_key=True)
     movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"))
+    date: Mapped[datetime.datetime]
 
 
 class GenresOrm(Base):
@@ -40,4 +43,12 @@ class GenresOrm(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
 
-    
+
+class OrdersOrm(Base):
+    __tablename__ = "orders"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"))
+    info: Mapped[str]
+    sum: Mapped[int]
+    created_at: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.now())
